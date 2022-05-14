e9c072438b2a7e023e40df70ee597742f044cb88
Andrey Melikhov <neeksdk@gmail.com>
2022-05-14 19:39:09 +0300

    F! - import DotWeen

---
+++ TraceEditor/Assets/neeksdk/Scripts/FigureTracer/FingerPointer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace neeksdk.Scripts.FigureTracer
+{
+    public class FingerPointer : MonoBehaviour
+    {
+        [SerializeField] private LineRenderer _path;
+        [SerializeField] private LineRenderer _pathReached;
+        [SerializeField] private Transform _transform;
+        [SerializeField] private Transform _fingerPointTransform;
+        
+        
+    }
+}
