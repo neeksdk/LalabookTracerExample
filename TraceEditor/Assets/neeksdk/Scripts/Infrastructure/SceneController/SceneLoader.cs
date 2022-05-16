30331bc1a78427d20eee7210d9322ec4b05ebf44
Andrey Melikhov <neeksdk@gmail.com>
2022-05-16 22:42:32 +0300

    B! - fix multiple scene loading issue

--- TraceEditor/Assets/neeksdk/Scripts/Infrastructure/SceneController/SceneLoader.cs
+++ TraceEditor/Assets/neeksdk/Scripts/Infrastructure/SceneController/SceneLoader.cs
@@ -8,10 +8,19 @@ namespace neeksdk.Scripts.Infrastructure.SceneController
     public class SceneLoader
     {
         private readonly ICoroutineRunner _coroutineRunner;
+        private bool _isLoading;
 
         public SceneLoader(ICoroutineRunner coroutineRunner) => _coroutineRunner = coroutineRunner;
 
-        public void Load(string name, Action onLoaded = null) => _coroutineRunner.StartCoroutine((LoadScene(name, onLoaded)));
+        public void Load(string name, Action onLoaded = null)
+        {
+            if (_isLoading)
+            {
+                return;
+            }
+            
+            _coroutineRunner.StartCoroutine((LoadScene(name, onLoaded)));
+        }
 
         private IEnumerator LoadScene(string nextScene, Action onLoaded = null)
         {
@@ -20,27 +29,15 @@ namespace neeksdk.Scripts.Infrastructure.SceneController
                 onLoaded?.Invoke();
                 yield break;
             }
-      
+
+            _isLoading = true;
             AsyncOperation waitNextScene = SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Single);
 
             while (!waitNextScene.isDone)
                 yield return null;
 
-            UnloadUnusedScenes();
             onLoaded?.Invoke();
-        }
-
-        private void UnloadUnusedScenes()
-        {
-            string activeSceneName = SceneManager.GetActiveScene().name;
-            for (int i = 0; i < SceneManager.sceneCount; i++)
-            {
-                Scene scene = SceneManager.GetSceneAt(i);
-                if (scene.name != activeSceneName)
-                {
-                    SceneManager.UnloadSceneAsync(scene);
-                }
-            }
+            _isLoading = false;
         }
     }
 }
