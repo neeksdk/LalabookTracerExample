054c44181ab60b5960febbdbcad0759a93803eb2
Andrey Melikhov <neeksdk@gmail.com>
2022-05-16 13:44:28 +0300

    B! - fix draw line glitch bug

--- TraceEditor/Assets/neeksdk/Scripts/FigureTracer/FingerPointer.cs
+++ TraceEditor/Assets/neeksdk/Scripts/FigureTracer/FingerPointer.cs
@@ -86,6 +86,7 @@ namespace neeksdk.Scripts.FigureTracer
                 {
                     Promise promise = new Promise();
                     _path.positionCount = index + 1;
+                    _path.SetPosition(index, _path.GetPosition(index - 1));
                     Vector3 nextDotPosition = _positionData[index].DotPosition;
                     float distance = Vector3.Distance(from, nextDotPosition);
                     
